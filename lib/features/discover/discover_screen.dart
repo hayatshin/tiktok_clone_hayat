5ef4790d5506417e61bddc39202600c5abc97501
hayatshin <hayat_shin@naver.com>
2023-06-21 00:51:34 +0900

    #14.5 수업내용

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/breakpoints.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
@@ -96,45 +97,52 @@ class _DiscoverScreenState extends State<DiscoverScreen>
                   ),
                 ),
                 Gaps.h10,
-                Expanded(
-                  child: SizedBox(
-                    width: size.width,
-                    height: Sizes.size48,
-                    child: TextField(
-                      onChanged: _onTypingTextCheck,
-                      focusNode: _focusNode,
-                      minLines: null,
-                      maxLines: null,
-                      expands: true,
-                      controller: _textEditingController,
-                      textInputAction: TextInputAction.newline,
-                      cursorColor: Theme.of(context).primaryColor,
-                      decoration: InputDecoration(
-                        // hintText: "input",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(Sizes.size10),
-                          borderSide: BorderSide.none,
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxWidth: size.width > Breakpoints.lg
+                        ? Breakpoints.sm
+                        : size.width * 0.8,
+                  ),
+                  child: Flexible(
+                    child: SizedBox(
+                      width: size.width,
+                      height: Sizes.size40,
+                      child: TextField(
+                        onChanged: _onTypingTextCheck,
+                        focusNode: _focusNode,
+                        minLines: null,
+                        maxLines: null,
+                        expands: true,
+                        controller: _textEditingController,
+                        textInputAction: TextInputAction.newline,
+                        cursorColor: Theme.of(context).primaryColor,
+                        decoration: InputDecoration(
+                          // hintText: "input",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(Sizes.size10),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Colors.grey.shade100,
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: Sizes.size10,
+                            vertical: Sizes.size10,
+                          ),
+                          suffixIconConstraints: const BoxConstraints(
+                            minWidth: 40,
+                            minHeight: 10,
+                          ),
+                          suffixIcon: _isWriting
+                              ? GestureDetector(
+                                  onTap: _onTapXmark,
+                                  child: const FaIcon(
+                                    FontAwesomeIcons.solidCircleXmark,
+                                    color: Colors.grey,
+                                    size: Sizes.size24,
+                                  ),
+                                )
+                              : null,
                         ),
-                        filled: true,
-                        fillColor: Colors.grey.shade100,
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: Sizes.size10,
-                          vertical: Sizes.size10,
-                        ),
-                        suffixIconConstraints: const BoxConstraints(
-                          minWidth: 40,
-                          minHeight: 10,
-                        ),
-                        suffixIcon: _isWriting
-                            ? GestureDetector(
-                                onTap: _onTapXmark,
-                                child: const FaIcon(
-                                  FontAwesomeIcons.solidCircleXmark,
-                                  color: Colors.grey,
-                                  size: Sizes.size24,
-                                ),
-                              )
-                            : null,
                       ),
                     ),
                   ),
@@ -184,76 +192,80 @@ class _DiscoverScreenState extends State<DiscoverScreen>
                 padding: const EdgeInsets.all(
                   Sizes.size6,
                 ),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: size.width > Breakpoints.lg ? 5 : 2,
                   crossAxisSpacing: Sizes.size10,
                   mainAxisSpacing: Sizes.size10,
                   childAspectRatio: 9 / 18,
                 ),
-                itemBuilder: (context, index) => Column(
-                  children: [
-                    Container(
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                          Sizes.size4,
+                itemBuilder: (context, index) => LayoutBuilder(
+                  builder: (context, constraints) => Column(
+                    children: [
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                            Sizes.size4,
+                          ),
+                        ),
+                        child: AspectRatio(
+                          aspectRatio: 9 / 13,
+                          child: FadeInImage.assetNetwork(
+                              placeholderFit: BoxFit.cover,
+                              fit: BoxFit.cover,
+                              placeholder: "assets/images/tree.jpg",
+                              image:
+                                  "https://media.istockphoto.com/id/944812540/ko/%EC%82%AC%EC%A7%84/%EC%82%B0-%ED%94%84%EB%A6%AC-%ED%8F%B0-%ED%83%80-%EB%8D%B8%EA%B0%80-%EB%8B%A4-%EC%84%AC-%EC%95%84%EC%A1%B0%EB%A0%88%EC%8A%A4-%EC%A0%9C%EB%8F%84.jpg?s=612x612&w=0&k=20&c=DQJ6eA0Wnxqt1yDdJChcoyuJ_5r0IQBduoIFZY0QV78="),
                         ),
                       ),
-                      child: AspectRatio(
-                        aspectRatio: 9 / 13,
-                        child: FadeInImage.assetNetwork(
-                            placeholderFit: BoxFit.cover,
-                            fit: BoxFit.cover,
-                            placeholder: "assets/images/tree.jpg",
-                            image:
-                                "https://media.istockphoto.com/id/944812540/ko/%EC%82%AC%EC%A7%84/%EC%82%B0-%ED%94%84%EB%A6%AC-%ED%8F%B0-%ED%83%80-%EB%8D%B8%EA%B0%80-%EB%8B%A4-%EC%84%AC-%EC%95%84%EC%A1%B0%EB%A0%88%EC%8A%A4-%EC%A0%9C%EB%8F%84.jpg?s=612x612&w=0&k=20&c=DQJ6eA0Wnxqt1yDdJChcoyuJ_5r0IQBduoIFZY0QV78="),
+                      Gaps.v10,
+                      const Text(
+                        "This is a very long caption for my tiktok that im upload just now currently.",
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: Sizes.size16 + Sizes.size2,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    Gaps.v10,
-                    const Text(
-                      "This is a very long caption for my tiktok that im upload just now currently.",
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: Sizes.size16 + Sizes.size2,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v8,
-                    DefaultTextStyle(
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      child: Row(
-                        children: [
-                          const CircleAvatar(
-                            radius: 12,
-                            backgroundImage: NetworkImage(
-                                "https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-8-avatar-2754583_120515.png"),
-                          ),
-                          Gaps.h4,
-                          const Expanded(
-                            child: Text(
-                              "Burnt_Pellet_Hayat_HyejungShin",
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                            ),
-                          ),
-                          Gaps.h4,
-                          FaIcon(
-                            FontAwesomeIcons.heart,
-                            size: Sizes.size16,
+                      Gaps.v8,
+                      if (constraints.maxWidth < 200 ||
+                          constraints.maxWidth > 250)
+                        DefaultTextStyle(
+                          style: TextStyle(
                             color: Colors.grey.shade600,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Gaps.h2,
-                          const Text(
-                            "2.5M",
-                          )
-                        ],
-                      ),
-                    )
-                  ],
+                          child: Row(
+                            children: [
+                              const CircleAvatar(
+                                radius: 12,
+                                backgroundImage: NetworkImage(
+                                    "https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-8-avatar-2754583_120515.png"),
+                              ),
+                              Gaps.h4,
+                              const Expanded(
+                                child: Text(
+                                  "Burnt_Pellet_Hayat_HyejungShin",
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                ),
+                              ),
+                              Gaps.h4,
+                              FaIcon(
+                                FontAwesomeIcons.heart,
+                                size: Sizes.size16,
+                                color: Colors.grey.shade600,
+                              ),
+                              Gaps.h2,
+                              const Text(
+                                "2.5M",
+                              )
+                            ],
+                          ),
+                        )
+                    ],
+                  ),
                 ),
               ),
               for (var tab in tabs.skip(1))
